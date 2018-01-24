77e873bcd17faf2e5fe1cf6cd26e6c88604a6f06
Masterflash <sxq@yandex.ru>
2018-01-24 19:19:56 +0300

    переработано сильно
    применен плагин zend-mvc-plugin-prg

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,6 +13,7 @@ use Statpage\Service\Statpage;
 use Kontakt\Form\KontaktForm;
 use Zend\Captcha;
 use Zend\Mail;
+use Zend\Http\PhpEnvironment\Response;
 
 
 class IndexController extends AbstractActionController
@@ -34,67 +35,59 @@ public function indexAction()
 {
 	$locale=$this->params('locale',$this->config["locale_default"]);
 	$default_locale=$this->config["locale_default"];
-	try
-	{
+	try {
+		
+		$prg = $this->prg();
+		if ($prg instanceof Response) {
+			//сюда попадаем когда форма отправлена, производим редирект
+			return $prg;
+		}
+
 		//переключим транслятор на нужную локаль
 		$this->translator->setLocale($locale);
 		
+		$view=new ViewModel();
 		//форма
 		//здесь создаем капчу исходя из настроек 
 		$options=$this->config["captcha"]["options"][$this->config["captcha"]["adapter"]];
 		$adapter= "\\".$this->config["captcha"]["adapter"];
 		$captcha=new $adapter($options);
 		
-		$view=new ViewModel();
-		
 		$form = new KontaktForm($captcha,$this->validatortranslator);
-		if ($this->getRequest()->isPost())
-			{
-				$form->setData($this->params()->fromPost());
-				if ($form->isValid()) 
-					{
-						//данные в норме
-						$info=$form->getData();
-						if ($_SERVER["REQUEST_TIME"]-$_SESSION["_kontakt_form_"] > 0)
-							{
-								$mess="Сообщение с сайта:\n\n". $info['message'].
-							       // "\n Имя: ". $info['name'].
-							        "\n E_mail: ".$info['email'];
-								//отправляем на почту сообщение
-								  if (mb_strpos($mess,"://",0,"UTF-8")===false)
-											{
-												$mail = new Mail\Message();
-												$mail->setEncoding("UTF-8");
-												$mail->setBody($mess);
-												$mail->setFrom($this->config["email_robot"]);
-												$mail->addTo($this->config["admin_emails"]);
-												$mail->setSubject("Сообщение с сайта (из контактов) ".$_SERVER["SERVER_NAME"]);
-												
-												$transport = new Mail\Transport\Sendmail();
-												$transport->send($mail);
-											}							
-							}
-						$this->flashMessenger()->addMessage("<h1>Сообщение успешно отправлено</h1>");
-						if ($default_locale!=$locale) 
-							{
-								return $this->redirect()->toRoute('kontakt_ru_RU',["locale"=>$locale]);
-							} 
-						return $this->redirect()->toRoute('kontakt_ru_RU');
-					}
+		
+		if ($prg === false){
+			//вывод страницы и формы
+			$view->setVariables(["locale"=>$locale,"form"=>$form]);
+			$_SESSION["_kontakt_form_"]=time();
+			return $view;
+		}
+
+		$form->setData($prg);
+
+		if ($form->isValid()) {
+			//данные в норме
+			$info=$form->getData();
+			if ($_SERVER["REQUEST_TIME"]-$_SESSION["_kontakt_form_"] > 0) {
+				$mess="Сообщение с сайта:\n\n". $info['message'].
+				// "\n Имя: ". $info['name'].
+				"\n E_mail: ".$info['email'];
+				//отправляем на почту сообщение, запрещаем пересылать URL
+				if (mb_strpos($mess,"://",0,"UTF-8")===false){
+					$mail = new Mail\Message();
+					$mail->setEncoding("UTF-8");
+					$mail->setBody($mess);
+					$mail->setFrom($this->config["email_robot"]);
+					$mail->addTo($this->config["admin_emails"]);
+					$mail->setSubject("Сообщение с сайта (из контактов) ".$_SERVER["SERVER_NAME"]);
+					$transport = new Mail\Transport\Sendmail();
+					$transport->send($mail);
+				}							
 			}
+			$view->setVariables(["message"=>"<h1>Сообщение успешно отправлено</h1>"]);
+			$view->setTemplate("kontakt/index/ok.phtml");
+		}
 		
-		//если был редирект, то будет сообщение в сессии, тогда выводим другой шаблон, без формы
-		$flashMessenger = $this->flashMessenger();
-		if ($flashMessenger->hasMessages()) 
-			{
-				$view->setVariables(["message"=>implode("",$flashMessenger->getMessages())]);
-				$view->setTemplate("kontakt/index/ok.phtml");
-				return $view;
-			}		
-		
-		//вывод страницы и формы
 		$view->setVariables(["locale"=>$locale,"form"=>$form]);
-		$_SESSION["_kontakt_form_"]=time();
 		return $view;
 	}
 	catch (\Exception $e) 
